133e7e41f07596d586fe73a882aadab80bcf5c0d
GeoInter <phantom.link777@gmail.com>
2022-06-04 19:44:51 +0200

    Add first css file and Fix displaying empty rows in table

--- src/main/java/thb/fbi/App.java
+++ src/main/java/thb/fbi/App.java
@@ -20,6 +20,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("simulator"), 640, 480);
+        scene.getStylesheets().add(this.getClass().getResource("temp.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
         stage.setTitle("ARMv8 Thumb Simulator");
