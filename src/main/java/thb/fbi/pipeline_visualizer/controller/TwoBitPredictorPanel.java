76054b728950491205a43e1189e6f50639aa3eb8
GeoInter <phantom.link777@gmail.com>
2023-07-27 14:58:03 +0200

    Add checkBox for hiding predictor table

--- src/main/java/thb/fbi/pipeline_visualizer/controller/TwoBitPredictorPanel.java
+++ src/main/java/thb/fbi/pipeline_visualizer/controller/TwoBitPredictorPanel.java
@@ -1,9 +1,12 @@
 package thb.fbi.pipeline_visualizer.controller;
 
 import java.awt.Dimension;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.HashMap;
 
 import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -13,16 +16,31 @@ import thb.fbi.pipeline_visualizer.pipeline.TwoBitPrecitionEntry;
 
 public class TwoBitPredictorPanel extends JPanel {
 
+    private JCheckBox showTableCheckBox;
     private JTable predictionTable;
     private JScrollPane scrollPane;
     private String[] columnNames = {"Code", "Prediction", "Target Address"};
 
     public TwoBitPredictorPanel() {
+        showTableCheckBox = new JCheckBox("show predictor table");
+        showTableCheckBox.setSelected(true);
+        showTableCheckBox.addItemListener(new ItemListener() {
+
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                scrollPane.setVisible(e.getStateChange() == 1);
+                System.out.println(scrollPane.isVisible());
+            }
+            
+        });
+
         predictionTable = new JTable(null, columnNames);
-        scrollPane = new JScrollPane(predictionTable);
         predictionTable.setPreferredScrollableViewportSize(new Dimension(200, 100));
         predictionTable.setFillsViewportHeight(true);
+        scrollPane = new JScrollPane(predictionTable);
+
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        this.add(showTableCheckBox);
         this.add(scrollPane);
     }
 
