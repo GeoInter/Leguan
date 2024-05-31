3167716203cbc78fc16e003d20eae40334c30ecd
GeoInter <phantom.link777@gmail.com>
2024-05-31 12:03:06 +0200

    Add machine code translation for condition codes

--- src/main/java/thb/fbi/leguan/instructions/ConditionalBranchInstruction.java
+++ src/main/java/thb/fbi/leguan/instructions/ConditionalBranchInstruction.java
@@ -36,7 +36,50 @@ public class ConditionalBranchInstruction extends Instruction {
         String s = "";
         s = MachineCodeTranslator.convertToMachineCode(opcode, 8);
         s += " " + MachineCodeTranslator.convertToMachineCode(args.getCond_Br_Address(), 19);
-        s += " " + MachineCodeTranslator.convertToMachineCode(args.getRt().getID(), 5);
+        if (args.getRt() == null) { // instructions that not use RT but condition codes like B.EQ, B.LT
+            s += " " + getMachineCodeForConditionCode(this.mnemonic);
+        } else { // instructions that use RT like CBNZ, CBZ
+            s += " " + MachineCodeTranslator.convertToMachineCode(args.getRt().getID(), 5);
+        }
+        return s;
+    }
+
+
+    private String getMachineCodeForConditionCode(String mnemonic) {
+        mnemonic = mnemonic.substring(2); // get actual condition
+        String s = "";
+        switch (mnemonic) {
+            case "EQ":
+                s = "00000";
+                break;
+            case "NE":
+                s = "00001";
+                break;
+            case "LT":
+                s = "01011";
+                break;
+            case "LE":
+                s = "01101";
+                break;
+            case "GT":
+                s = "01100";
+                break;
+            case "GE":
+                s = "01010";
+                break;
+            case "MI":
+                s = "00100";
+                break;
+            case "PL":
+                s = "00101";
+                break;
+            case "VS":
+                s = "00110";
+                break;
+            case "VC":
+                s = "00111";
+                break;
+        }
         return s;
     }
 
