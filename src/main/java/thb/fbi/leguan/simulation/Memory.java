d2eaec4bb099c04251e25ecb957e4636ed207f96
GeoInter <phantom.link777@gmail.com>
2024-07-18 17:30:20 +0200

    Fix dynamic data segment start adress

--- src/main/java/thb/fbi/leguan/simulation/Memory.java
+++ src/main/java/thb/fbi/leguan/simulation/Memory.java
@@ -12,9 +12,9 @@ public class Memory {
     private static TreeMap<Long, Byte> dataStorage = new TreeMap<Long, Byte>(); // address used as key
     /** lock addresses; boolean indicates if changed by other store instruction than STXR */
     private static HashMap<Long, Boolean> lockStorage = new HashMap<Long, Boolean>();
-    /** start of the dynamic data segment (1000.0000 hex) */
-    public final static long DATA_SEGMENT_START = 40000;
-    /** start of the code/ text segment (40.000 hex) */
+    /** start of the dynamic data segment (1000 0000 hex) */
+    public final static long DATA_SEGMENT_START = 268435456;
+    /** start of the code/ text segment (40 0000 hex) */
     public final static int CODE_SEGMENT_START = 4194304; 
 
     private static MemoryObserver observer;
