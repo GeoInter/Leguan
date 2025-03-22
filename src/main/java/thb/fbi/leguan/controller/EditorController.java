ed19290a2e60ecba553741f5b7ec1c2d19724498
GeoInter <phantom.link777@gmail.com>
2025-03-22 11:09:14 +0100

    Put editor related code in own class

---
+++ src/main/java/thb/fbi/leguan/controller/EditorController.java
@@ -0,0 +1,152 @@
+package thb.fbi.leguan.controller;
+
+import java.time.Duration;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.ExecutorService;
+import java.util.function.IntFunction;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.fxmisc.flowless.VirtualizedScrollPane;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
+import org.fxmisc.richtext.model.PlainTextChange;
+import org.fxmisc.richtext.model.StyleSpans;
+import org.fxmisc.richtext.model.StyleSpansBuilder;
+import org.reactfx.EventStream;
+import org.reactfx.util.Try;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+import thb.fbi.leguan.parser.SyntaxHighlighter;
+import thb.fbi.leguan.parser.antlr.LegV8Lexer;
+import thb.fbi.leguan.parser.antlr.LegV8Parser;
+import thb.fbi.leguan.simulation.Simulator;
+import thb.fbi.leguan.simulation.SimulatorSingleton;
+import thb.fbi.leguan.utility.ExecutorServiceProvider;
+
+public class EditorController {
+
+    @FXML
+    StackPane codeStackPane;
+
+    private Circle assembleIndicator;
+
+    private CodeArea codeArea;
+
+    private EditorCanvas editorCanvas;
+
+    private VirtualizedScrollPane<CodeArea> codeAreaScrollPane;
+
+    private ExecutorService executorService;
+
+    private Simulator simulator = SimulatorSingleton.getSimulator();
+    
+    @FXML
+    public void initialize() {
+        executorService = ExecutorServiceProvider.getExecutorService();
+
+        codeArea = new CodeArea();
+        codeAreaScrollPane = new VirtualizedScrollPane<CodeArea>(codeArea);
+        codeStackPane.getChildren().add(codeAreaScrollPane);
+
+        IntFunction<String> lineNumberFormat = (digits -> " %" + digits + "d\t");
+        codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea, lineNumberFormat));
+        codeArea.setLineHighlighterOn(true);
+        codeArea.textProperty().addListener(new ChangeListener<String>() {
+
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                simulator.getIsCodeChanged().set(true);
+                assembleIndicator.setId("codeStatus_changed");
+            }
+
+        });
+
+        EventStream<PlainTextChange> textChanges = codeArea.plainTextChanges();
+
+        textChanges.successionEnds(Duration.ofMillis(500))
+                .supplyTask(this::computeHighlightingAsync)
+                .awaitLatest(textChanges)
+                .subscribe(this::applyHighlighting);
+
+        editorCanvas = new EditorCanvas(codeArea, codeAreaScrollPane);
+        codeStackPane.getChildren().add(editorCanvas);
+
+        codeArea.addEventFilter(ScrollEvent.ANY, scroll -> {
+            editorCanvas.reposition(codeAreaScrollPane.getEstimatedScrollY(),
+                    codeAreaScrollPane.getTotalHeightEstimate(), scroll.getDeltaY());
+        });
+    }
+
+    private Task<StyleSpans<Collection<String>>> computeHighlightingAsync() {
+        String code = codeArea.getText();
+
+        Task<StyleSpans<Collection<String>>> task = new Task<StyleSpans<Collection<String>>>() {
+            @Override
+            protected StyleSpans<Collection<String>> call() throws Exception {
+                return computeHighlighting(code);
+            }
+        };
+        executorService.execute(task);
+        return task;
+    }
+
+    private static StyleSpans<Collection<String>> computeHighlighting(String text) {
+        if (text.length() > 0) {
+            CharStream input = CharStreams.fromString(text);
+            LegV8Lexer lexer = new LegV8Lexer(input);
+            lexer.getErrorListeners().clear();
+            // parse
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            LegV8Parser parser = new LegV8Parser(tokens);
+            parser.getErrorListeners().clear();
+            parser.setBuildParseTree(true);
+            LegV8Parser.MainContext mainContext = parser.main();
+
+            ParseTreeWalker walker = new ParseTreeWalker();
+            SyntaxHighlighter syntaxHighlighter = new SyntaxHighlighter(text.length());
+            walker.walk(syntaxHighlighter, mainContext);
+            return syntaxHighlighter.getStyles();
+        } else {
+            StyleSpansBuilder<Collection<String>> objectStyleSpansBuilder = new StyleSpansBuilder<>();
+            objectStyleSpansBuilder.add(Collections.emptyList(), 0);
+            return objectStyleSpansBuilder.create();
+        }
+    }
+
+    private void applyHighlighting(Try<StyleSpans<Collection<String>>> taskTry) {
+        StyleSpans<Collection<String>> highlighting = taskTry.get();
+        if (highlighting.getSpanCount() > 0) {
+            codeArea.setStyleSpans(0, highlighting);
+        }
+    }
+
+    public CodeArea getCodeArea() {
+        return this.codeArea;
+    }
+
+    public String getCodeAreaText() {
+        return codeArea.getText();
+    }
+
+    public void setAssembleIndicator(Circle assembleIndicator) {
+        this.assembleIndicator = assembleIndicator;
+    }
+
+    public void setIsEnabledLineHighlighter(boolean isEnabled) {
+        editorCanvas.setVisible(isEnabled);
+    }
+
+    public void setLineNumber(int lineNumber) {
+        editorCanvas.setLineNumber(lineNumber);
+    }
+}
--- src/main/java/thb/fbi/leguan/controller/SimulatorController.java
+++ src/main/java/thb/fbi/leguan/controller/SimulatorController.java
@@ -1,29 +1,8 @@
 package thb.fbi.leguan.controller;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Locale;
-import java.util.concurrent.ExecutorService;
-import java.util.function.IntFunction;
-import java.util.Collection;
-import java.util.Collections;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import org.fxmisc.flowless.VirtualizedScrollPane;
-import org.fxmisc.richtext.CodeArea;
-import org.fxmisc.richtext.LineNumberFactory;
-import org.fxmisc.richtext.model.PlainTextChange;
-import org.fxmisc.richtext.model.StyleSpans;
-import org.fxmisc.richtext.model.StyleSpansBuilder;
-import org.reactfx.EventStream;
-import org.reactfx.util.Try;
-
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -40,21 +19,15 @@ import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
 import javafx.stage.WindowEvent;
 import thb.fbi.leguan.App;
 import thb.fbi.leguan.parser.ParsingError;
-import thb.fbi.leguan.parser.SyntaxHighlighter;
-import thb.fbi.leguan.parser.antlr.LegV8Lexer;
-import thb.fbi.leguan.parser.antlr.LegV8Parser;
 import thb.fbi.leguan.simulation.FlagRegister;
 import thb.fbi.leguan.simulation.Memory;
 import thb.fbi.leguan.simulation.Simulator;
 import thb.fbi.leguan.simulation.SimulatorSingleton;
-import thb.fbi.leguan.utility.ExecutorServiceProvider;
 import thb.fbi.leguan.utility.FileManager;
 import thb.fbi.leguan.utility.I18N;
 import thb.fbi.leguan.utility.ILeguanTools;
@@ -71,13 +44,15 @@ public class SimulatorController {
     RegisterPaneController registerPaneController; // bound via registerPane + Controller (automatically)
     @FXML 
     MemoryController memoryController; // bound via memory + Controller (automatically)
+    @FXML 
+    EditorController editorController;
     @FXML
     Parent registerPane;
     @FXML
     Parent memory;
-
     @FXML
-    StackPane codeStackPane;
+    Parent editor;
+
     @FXML
     SplitPane splitPane;
     @FXML
@@ -88,8 +63,6 @@ public class SimulatorController {
     SplitPane textSplitpane;
     @FXML
     AnchorPane rightSideAnchorPane;
-    @FXML
-    AnchorPane codeAreaAnchorPane;
     @FXML
     TabPane tabPane;
     @FXML
@@ -124,61 +97,18 @@ public class SimulatorController {
     @FXML
     Button stepBackwardButton;
     @FXML
-    Circle assembleIdicator;
-
-    private CodeArea codeArea;
+    Circle assembleIndicator;
 
     private Simulator simulator = SimulatorSingleton.getSimulator();
-    private ExecutorService executorService;
-    private EditorCanvas editorCanvas;
-    private VirtualizedScrollPane<CodeArea> codeAreaScrollPane;
 
     private ILeguanTools pipelineVisualizer;
 
     @FXML
     public void initialize() {
-        executorService = ExecutorServiceProvider.getExecutorService();
-        codeArea = new CodeArea();
-        codeAreaScrollPane = new VirtualizedScrollPane<CodeArea>(codeArea);
-        codeStackPane.getChildren().add(codeAreaScrollPane);
-        IntFunction<String> lineNumberFormat = (digits -> " %" + digits + "d\t");
-        codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea, lineNumberFormat));
-        codeArea.setLineHighlighterOn(true);
-        codeArea.textProperty().addListener(new ChangeListener<String>() {
-
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                simulator.getIsCodeChanged().set(true);
-                assembleIdicator.setId("codeStatus_changed");
-            }
-
-        });
-
-        EventStream<PlainTextChange> textChanges = codeArea.plainTextChanges();
-
-        textChanges.successionEnds(Duration.ofMillis(500))
-                .supplyTask(this::computeHighlightingAsync)
-                .awaitLatest(textChanges)
-                .subscribe(this::applyHighlighting);
-
-        editorCanvas = new EditorCanvas(codeArea, codeAreaScrollPane);
-        codeStackPane.getChildren().add(editorCanvas);
-
-        codeArea.addEventFilter(ScrollEvent.ANY, scroll -> {
-            editorCanvas.reposition(codeAreaScrollPane.getEstimatedScrollY(),
-                    codeAreaScrollPane.getTotalHeightEstimate(), scroll.getDeltaY());
-        });
+        editorController.setAssembleIndicator(assembleIndicator);
 
         // prevent rightside to resize (change divider position) when maximazing
         SplitPane.setResizableWithParent(rightSideAnchorPane, false);
-
-        // move divider to make room for the combined tab
-        combinedTab.selectedProperty().addListener((obs, wasSelected, isSelected) -> {
-            if (isSelected) {
-                splitPane.setDividerPositions(0.5);
-                combinedTab_SplitPane.setDividerPositions(0.5);
-            }
-        });
 
         // tabs fill header of tabPane
         // BUG: Switching Tabs (which are filling the tabPane) leaves empty extra space
@@ -214,7 +144,7 @@ public class SimulatorController {
                 getClass().getResourceAsStream("/thb/fbi/leguan/images/backward.png"));
         stepBackwardButton.setGraphic(new ImageView(stepBackwardButtonImage));
 
-        FileManager.init(codeArea);
+        FileManager.init(editorController.getCodeArea());
 
         pipelineVisualizer = new PipelineVisualizerAdapter();
         pipelineVisualizer.addInitCSS("/thb/fbi/leguan/css/base.css");
@@ -228,7 +158,7 @@ public class SimulatorController {
 
     @FXML
     private void assembleCode() {
-        if (simulator.parse(codeArea.getText())) {
+        if (simulator.parse(editorController.getCodeAreaText())) {
             setConsoleText(simulator.getErrors());
             stepForwardButton.setDisable(false);
             // stepBackwardButton.setDisable(false);
@@ -237,12 +167,12 @@ public class SimulatorController {
             memoryController.updateCode(simulator.getArmProgram());
 
             // Update Tool
-            pipelineVisualizer.updateCode(simulator.getArmProgram(), codeArea.getText());
+            pipelineVisualizer.updateCode(simulator.getArmProgram(), editorController.getCodeAreaText());
 
-            assembleIdicator.setId("codeStatus_assembled");
+            assembleIndicator.setId("codeStatus_assembled");
         } else {
             setConsoleText(simulator.getErrors());
-            editorCanvas.setLineNumber(-1);
+            editorController.setLineNumber(-1);
             stepForwardButton.setDisable(true);
             // stepBackwardButton.setDisable(true);
         }
@@ -250,14 +180,14 @@ public class SimulatorController {
 
     @FXML
     private void runCode() {
-        if (simulator.parse(codeArea.getText())) {
+        if (simulator.parse(editorController.getCodeAreaText())) {
             setConsoleText(simulator.getErrors());
             memoryController.updateCode(simulator.getArmProgram()); // Update Memory Code Segment Table
             simulator.runAllInstructions();
         } else {
             setConsoleText(simulator.getErrors());
         }
-        editorCanvas.setLineNumber(-1);
+        editorController.setLineNumber(-1);
     }
 
     /**
@@ -270,13 +200,13 @@ public class SimulatorController {
         simulator.reset();
         registerPaneController.clearRegisterHighlighting();
         registerPaneController.clearFlagHighlighting();
-        editorCanvas.setLineNumber(0);
+        editorController.setLineNumber(0);
     }
 
     @FXML
     private void stepForward() {
         int lineNumber = simulator.forwardStep();
-        editorCanvas.setLineNumber(lineNumber);
+        editorController.setLineNumber(lineNumber);
     }
 
     // run clear line
@@ -336,49 +266,6 @@ public class SimulatorController {
         }
     }
 
-    private Task<StyleSpans<Collection<String>>> computeHighlightingAsync() {
-        String code = codeArea.getText();
-
-        Task<StyleSpans<Collection<String>>> task = new Task<StyleSpans<Collection<String>>>() {
-            @Override
-            protected StyleSpans<Collection<String>> call() throws Exception {
-                return computeHighlighting(code);
-            }
-        };
-        executorService.execute(task);
-        return task;
-    }
-
-    private static StyleSpans<Collection<String>> computeHighlighting(String text) {
-        if (text.length() > 0) {
-            CharStream input = CharStreams.fromString(text);
-            LegV8Lexer lexer = new LegV8Lexer(input);
-            lexer.getErrorListeners().clear();
-            // parse
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            LegV8Parser parser = new LegV8Parser(tokens);
-            parser.getErrorListeners().clear();
-            parser.setBuildParseTree(true);
-            LegV8Parser.MainContext mainContext = parser.main();
-
-            ParseTreeWalker walker = new ParseTreeWalker();
-            SyntaxHighlighter syntaxHighlighter = new SyntaxHighlighter(text.length());
-            walker.walk(syntaxHighlighter, mainContext);
-            return syntaxHighlighter.getStyles();
-        } else {
-            StyleSpansBuilder<Collection<String>> objectStyleSpansBuilder = new StyleSpansBuilder<>();
-            objectStyleSpansBuilder.add(Collections.emptyList(), 0);
-            return objectStyleSpansBuilder.create();
-        }
-    }
-
-    private void applyHighlighting(Try<StyleSpans<Collection<String>>> taskTry) {
-        StyleSpans<Collection<String>> highlighting = taskTry.get();
-        if (highlighting.getSpanCount() > 0) {
-            codeArea.setStyleSpans(0, highlighting);
-        }
-    }
-
     /**
      * sets the given Locale in the I18N class
      * 
@@ -402,7 +289,7 @@ public class SimulatorController {
 
     @FXML
     private void enableLineHighlighter(boolean isEnabled) {
-        editorCanvas.setVisible(isEnabled);
+        editorController.setIsEnabledLineHighlighter(isEnabled);
     }
 
     /**
